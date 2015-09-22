3e9976baa049eb27adc27dbe60cdce93c0205c2f
Abby Mullen <abby@lincolnmullen.com>
2015-09-22 08:55:38 -0400

    Fixed line break problem

--- text_breaking_VT.py
+++ text_breaking_VT.py
@@ -12,5 +12,7 @@ for row in a.iterrows():
     for key in ["uid","cluster","size","date","id","series","title","url","ed","issue","corpus","text"]: # This can be longer
         metadata[key] = indexed[key]
     metadata["filename"] = str(indexed["uid"])
+    metadata["searchstring"] = str(indexed["url"])
+    metadata["raw_text"] = str(indexed["text"].replace("\n"," ")) #to get rid of newlines in metadata too
     metadata_file.write(json.dumps(metadata) + "\n")
     texts.write("%d\t%s\n" %(indexed["uid"],indexed["text"].replace("\n"," ")))
